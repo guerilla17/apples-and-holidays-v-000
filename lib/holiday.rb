1fa1194f5feaca03df8af0632f516c44c3289b13
Gigi M <mmgamao1@gmail.com>
2019-03-15 20:33:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,8 +51,6 @@ def all_winter_holiday_supplies(holiday_hash)
 end
   # return an array of all of the supplies that are used in the winter season
 
-end
-
 
 
 def all_supplies_in_holidays(holiday_hash)
